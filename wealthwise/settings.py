a2c416b67aef0073e7f7897c486cddaa256acdf5
ttgannon <ga1179@nyu.edu>
2025-04-07 11:42:35 -0400

    removing gemini api key?

--- wealthwise/settings.py
+++ wealthwise/settings.py
@@ -97,6 +97,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
-
-# Gemini API Key
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY') 
